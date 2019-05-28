2dee5fa6f60257746c0008d974926cb054d2d6bd
bigbirds <bigbirds83@gmail.com>
2019-05-28 17:50:28 +0900

    첫 커밋

---
+++ info.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>
+    <?php echo $_SERVER['SERVER_ADDR'];?>
+    123
+    123
+    123
+    123
+</div>
+</body>
+</html>
