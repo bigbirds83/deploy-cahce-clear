99cce6232956db359ba094ca87f80d9dd25373aa
bigbirds <bigbirds83@gmail.com>
2019-06-11 17:35:47 +0900

    스크립트 추가

---
+++ scripts/delete_www_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Here is where you'd want to start your http daemon. For example:
+rm -rf /var/www/html/*
+exit $?
+
+# In this case, since it's just a placeholder, we don't need to do anything.
+exit 0
